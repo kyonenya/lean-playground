b41bf9b4ace0eeae9bb0707cf262c56495331785
kyonenya <kyon.musica@gmail.com>
2026-07-20 18:34:07 +0900

    feat: 含意と≠
    A≠B: A=B → False

--- NNG.lean
+++ NNG.lean
@@ -41,12 +41,13 @@ opaque add : MyNat → MyNat → MyNat
 instance : Add MyNat where
   add := MyNat.add
 
-/- ここから開始 -/
+---- Tutorial World ----
 
 axiom one_eq_succ_zero : (1 : MyNat) = MyNat.succ 0
--- axiom two_eq_succ_one : (2 : MyNat) = MyNat.succ 1
--- axiom three_eq_succ_two : (3 : MyNat) = MyNat.succ 2
--- axiom four_eq_succ_three : (4 : MyNat) = MyNat.succ 3
+axiom two_eq_succ_one : (2 : MyNat) = MyNat.succ 1
+axiom three_eq_succ_two : (3 : MyNat) = MyNat.succ 2
+axiom four_eq_succ_three : (4 : MyNat) = MyNat.succ 3
+axiom five_eq_succ_four : (5 : MyNat) = MyNat.succ 4
 
 axiom add_zero (n : MyNat) : n + 0 = n
 
@@ -57,6 +58,8 @@ theorem succ_eq_add_one (n : MyNat) : MyNat.succ n = n + 1 := by
   rw [add_succ]
   rw [add_zero]
 
+---- Addition World ----
+
 theorem zero_add (n : MyNat) : 0 + n = n := by
   induction n with
   | zero =>
@@ -65,18 +68,117 @@ theorem zero_add (n : MyNat) : 0 + n = n := by
     rw [add_succ]
     rw [ih]
 
-theorem succ_add (a b : MyNat) : MyNat.succ a + b = MyNat.succ (a + b) := by
+theorem succ_add (a b : MyNat) :
+  MyNat.succ a + b = MyNat.succ (a + b) := by
   induction b with
   | zero =>
     rw [add_zero, add_zero]
   | succ b ih =>
-    rw [add_succ, add_succ]
+    rw [add_succ, add_succ] -- succ outside
     rw [ih]
 
-theorem add_comm (a b : MyNat) : a + b = b + a := by
+theorem add_comm (a b : MyNat) :
+  a + b = b + a := by
   induction b with
     | zero =>
       rw [add_zero, zero_add]
     | succ b ih =>
-      rw [add_succ, succ_add]  -- symmetry
+      rw [add_succ, succ_add] -- symmetry
       rw [ih]
+
+theorem add_assoc (a b c : MyNat) :
+  a + b + c = a + (b + c) := by
+  induction c with
+  | zero =>
+    rw [add_zero, add_zero]
+  | succ c ih =>
+    rw [add_succ, add_succ, add_succ] -- succ outside
+    rw [ih]
+
+theorem add_right_com (a b c : MyNat) :
+  (a + b) + c = (a + c) + b := by
+  rw [add_assoc]
+  rw [add_comm b c]
+  rw [← add_assoc]
+
+---- Implication World ----
+
+theorem _apply_and_exact (x y : MyNat)
+  (h1 : x = 37)
+  (h2 : x = 37 → y = 42) :
+  y = 42 := by
+  have h3 : y = 42 := h2 h1 -- apply h2 at h1
+  exact h3
+
+/--
+ - 必要なのが Y なら、`succ_inj h` のように X を渡す必要がある
+ - 必要なのが X → Y なら、関数 `succ_inj` を引数省略で書ける
+ -/
+axiom succ_inj {a b : MyNat} :
+  MyNat.succ a = MyNat.succ b → a = b
+
+theorem _example_succ_inj (x : MyNat)
+  (h : x + 1 = 4) :
+  x = 3 := by
+  rw [← succ_eq_add_one] at h
+  rw [four_eq_succ_three] at h
+  have h2 : x = 3 := succ_inj h
+  exact h2
+
+theorem _apply_backwards (x : MyNat)
+  (h : x + 1 = 4) :
+  x = 3 := by
+  apply succ_inj -- this => x = 3
+  rw [succ_eq_add_one, ← four_eq_succ_three]
+  exact h
+
+theorem _example_intro (x : MyNat) :
+  x = 24 → x = 24 := by
+  intro h
+  exact h
+
+theorem _apply_backwards_2 (x y : MyNat) :
+  x + 1 = y + 1 → x = y := by
+  intro h
+  apply succ_inj
+  repeat rw [succ_eq_add_one]
+  exact h
+
+theorem _eq_and_not_eq (x y : MyNat)
+  (h1 : x = y)
+  (h2 : x ≠ y) : -- x = y → False
+  False := by
+  -- have hf : False := h2 h1
+  -- exact hf
+  apply h2
+  exact h1
+
+/--
+ - HOC
+ - x → (0 = succ x) → False
+ -/
+axiom zero_ne_succ (x : MyNat) :
+  0 ≠ succ x -- 0 = succ x → False
+
+theorem zero_ne_one :
+  (0 : MyNat) ≠ (1 : MyNat) := by -- 0 = 1 → False
+  rw [one_eq_succ_zero]
+  exact zero_ne_succ 0
+
+theorem one_ne_zero :
+  (1 : MyNat) ≠ 0 := by
+  symm
+  exact zero_ne_one
+
+theorem _two_ne_three :
+  2 + 2 ≠ (3 : MyNat) := by
+  repeat rw [three_eq_succ_two, two_eq_succ_one, one_eq_succ_zero]
+  repeat rw [add_succ]
+  rw [add_zero]
+  intro h
+  have h2 : succ (succ (succ 0)) = succ (succ 0) := succ_inj h
+  replace h2 : succ (succ 0) = succ 0 := succ_inj h2
+  replace h2 : succ 0 = 0 := succ_inj h2
+  symm at h2
+  replace h2 : False := zero_ne_succ 0 h2 -- HOC
+  exact h2
