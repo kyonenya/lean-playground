585fbb8f3c70739b831ddffb2fc7e9e970b3a95d
kyonenya <kyon.musica@gmail.com>
2026-08-23 09:38:56 +0900

    feat: 掛け算の導入

--- LeanBook/Ch4_NatCommMonoid.lean
+++ LeanBook/Ch4_NatCommMonoid.lean
@@ -1,11 +1,14 @@
 import LeanBook.Ch2_NaturalNumber
 
+-- ## TypeClass
+
 -- `OfNat` 型クラスの `インスタンス`
 @[default_instance]
 instance (n : Nat) : OfNat MyNat n where
   ofNat := MyNat.ofNat n
 -- 文字 0, 1, 2... が使えるようになる
 
+-- [記号そのものはLeanが知っているけれど、その型で何を意味するかはインスタンス次第]
 instance : Add MyNat where
   add := MyNat.add
 
@@ -40,6 +43,8 @@ theorem MyNat.add_succ (m n : MyNat) :
   m + .succ n = .succ (m + n) := by
   rfl
 
+-- ## Induction
+
 theorem MyNat.zero_add (n : MyNat) : 0 + n = n := by
   induction n with
   | zero =>
@@ -60,6 +65,8 @@ example (n : MyNat) :
     rw [MyNat.add_succ]
     rw [ih]
 
+-- ## Simp
+
 example (n : MyNat) : 0 + (n + 0) = n := by
   -- 最初はsimp で証明できない
   fail_if_success simp
@@ -79,6 +86,7 @@ example : MyNat.zero = 0 := by
   -- simp に手動で命題を渡しても証明できる
   simp [MyNat.ctor_eq_zero]
 
+attribute [simp] MyNat.ctor_eq_zero
 attribute [simp] MyNat.add_succ
 
 example (m n : MyNat) (h : m + n + 0 = n + m) : m + n = n + m := by
@@ -103,5 +111,55 @@ example (n : MyNat) : 2 + n = n + 2 := by
   induction n with
   | zero => rfl
   | succ n' ih =>
-    -- 普段登録されている simp ルールに加えて、今回だけ帰納法の仮定 ih も単純化ルールとして使ってくれ
+    -- [普段登録されているsimpルールに加えて、今回だけ帰納法の仮定ihも使ってくれ]
     simp [ih]
+
+-- ## AcRfl
+
+theorem MyNat.add_comm (m n : MyNat) : m + n = n + m := by
+  induction n with
+  | zero =>
+    simp only [ctor_eq_zero, add_zero, zero_add]
+  | succ n' ih =>
+    simp only [add_succ, succ_add, succ.injEq]
+    exact ih
+
+-- [inductionの帰納型から自動生成される補題]
+#check MyNat.succ.injEq -- (n.succ = m.succ) = (n = m)
+#check MyNat.noConfusion -- zero ≠ succ n
+
+theorem MyNat.add_assoc (l m n : MyNat) : l + m + n = l + (m + n) := by
+  induction n with
+  -- | zero => simp only [ctor_eq_zero, add_zero]
+  -- 第二引数が 0 なので足し算の定義から従う
+  | zero => rfl
+  | succ n ih =>
+    simp only [add_succ, succ.injEq]
+    exact ih
+
+-- `ac_rfl` というタクティクに結合・交換法則を登録する
+instance : Std.Associative (α := MyNat) (· + ·) where
+  assoc := MyNat.add_assoc
+instance : Std.Commutative (α := MyNat) (· + ·) where
+  comm := MyNat.add_comm
+
+example (l m n : MyNat) : l + m + n + 3 = m + (l + n) + 3 := by
+  ac_rfl
+
+example (l m n : MyNat) : l + (1 + m) + n = m + (l + n) + 1 := by
+  ac_rfl
+
+-- ## BetterInduction
+
+/-- 帰納法を見やすくする
+- MyNat.zero → 0
+- MyNat.succ → +1
+-/
+def MyNat.recAux.{u} {motive : MyNat → Sort u}
+  (zero : motive 0)
+  (succ : (n : MyNat) → motive n → motive (n + 1)) (t : MyNat) : motive t :=
+  match t with
+  | .zero => zero
+  | .succ n => succ n (MyNat.recAux zero succ n)
+
+attribute [induction_eliminator] MyNat.recAux
---
+++ LeanBook/Ch5_Mult.lean
@@ -0,0 +1,53 @@
+import LeanBook.Ch4_NatCommMonoid
+
+def MyNat.mul (m n : MyNat) :=
+  match n with
+  | 0 => 0
+  | n + 1 => MyNat.mul m n + m
+
+instance : Mul MyNat where
+  mul := MyNat.mul
+
+#eval 2 * 3
+
+/-- 右のオペランドにある(· + 1) の消去
+式が単純になるわけではないので simp はつけない（つけても意味がない）
+-/
+theorem MyNat.mul_add_one (m n : MyNat) : m * (n + 1) = m * n + m := by
+  rfl
+
+/-- 左のオペランドにある(· + 1) の消去-/
+theorem MyNat.add_one_mul (m n : MyNat) : (m + 1) * n = m * n + n := by
+  induction n with
+  | zero => rfl
+  | succ n ih => calc
+    _ = (m + 1) * (n + 1) := by rfl
+    _ = (m + 1) * n + (m + 1) := by rw [MyNat.mul_add_one]
+    _ = m * n + n + (m + 1) := by rw [ih]
+    _ = m * n + m + (n + 1) := by ac_rfl
+    _ = m * (n + 1) + (n + 1) := by rw [MyNat.mul_add_one]
+
+-- ## 0 や1 を掛けたときの性質
+
+/-- 右から 0 を掛けても0 -/
+@[simp] theorem MyNat.mul_zero (m : MyNat) : m * 0 = 0 := by
+  rfl
+
+/-- 左から 0 を掛けても0 -/
+@[simp] theorem MyNat.zero_mul (n : MyNat) : 0 * n = 0 := by
+  induction n with
+  | zero => rfl
+  | succ n ih =>
+    simp only [mul_add_one, add_zero, ih]
+
+/-- 右から 1 を掛けても変わらない-/
+@[simp] theorem MyNat.mul_one (n : MyNat) : n * 1 = n := calc
+  _ = n * (0 + 1) := by simp only [zero_add]
+  _ = n * 0 + n := by rw [MyNat.mul_add_one]
+  _ = n := by simp only [mul_zero, zero_add]
+
+/-- 左から1 を掛けても変わらない -/
+@[simp] theorem MyNat.one_mul (n : MyNat) : 1 * n = n := calc
+_ = (0 + 1) * n := by simp
+_ = 0 * n + n := by rw [MyNat.add_one_mul]
+_ = n := by simp
