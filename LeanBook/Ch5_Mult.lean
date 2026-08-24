2f24708765886b216fbbaf1b5395d729b81bc669
kyonenya <kyon.musica@gmail.com>
2026-08-25 01:47:56 +0900

    feat: 第5章まで

--- LeanBook/Ch5_Mult.lean
+++ LeanBook/Ch5_Mult.lean
@@ -95,3 +95,44 @@ instance : Std.Associative (α := MyNat) (· * ·) where
 
 instance : Std.Commutative (α := MyNat) (· * ·) where
   comm := MyNat.mul_comm
+
+example (m n : MyNat) : m * n * n * m = m * m * n * n := by
+  ac_rfl
+
+example (m n : MyNat) : (m + n) * (m + n) = m * m + 2 * m * n + n * n := by
+  rw [show 2 = 1 + 1 from rfl]
+  simp [MyNat.add_mul, MyNat.mul_add]
+  ac_rfl
+
+/-- 数値リテラルを1 + 1 + ⋯ + 1 に分解するための補題 -/
+theorem unfoldNatLit (x : Nat)
+: (OfNat.ofNat (x + 2) : MyNat) = (OfNat.ofNat (x + 1) : MyNat) + 1 :=
+  rfl
+
+/-- 自然数を1 + 1 + ⋯ + 1 に分解する -/
+macro "expand_num" : tactic => `(tactic| focus
+  try simp only [unfoldNatLit]
+  -- 標準のNat の和を簡単な形にする
+  try simp only [Nat.reduceAdd]
+  -- OfNat.ofNat を消す
+  try dsimp only [OfNat.ofNat]
+  try simp only [
+    show MyNat.ofNat 1 = 1 from rfl,
+    show MyNat.ofNat 0 = 0 from rfl
+  ]
+)
+
+/-- 分配法則を適用して足し算を式の外側に持ってくるタクティク -/
+macro "distrib" : tactic => `(tactic| focus
+  expand_num
+  try simp only [MyNat.mul_add, MyNat.add_mul]
+  try simp only [MyNat.mul_zero, MyNat.zero_mul, MyNat.mul_one, MyNat.one_mul]
+  try ac_rfl
+)
+
+example (m n : MyNat) : m * n + n * n = (m + n) * n := by
+  distrib
+
+example (n : MyNat) : ∃ s t : MyNat, s * t = n * n + 8 * n + 16 := by
+  exists n + 4, n + 4
+  distrib
