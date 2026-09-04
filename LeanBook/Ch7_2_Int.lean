4b6a75f1f192a38cc7b923430522eb6bc25cd79c
kyonenya <kyon.musica@gmail.com>
2026-09-05 03:39:55 +0900

    feat: -表記と練習問題

--- LeanBook/Ch7_2_Int.lean
+++ LeanBook/Ch7_2_Int.lean
@@ -64,3 +64,58 @@ instance {n : Nat} : OfNat MyInt n where
   ofNat := MyInt.ofNat n
 
 #check (4 : MyInt)
+
+-- ### マイナス記号を作る
+
+-- 整数の符号反転はペアの左右を交換すればよい
+-- - ⟦(m, n)⟧ = ⟦(n, m)⟧
+
+-- 代表元について左右を交換する関数
+def PreInt.neg : PreInt → MyInt
+  | (m, n) => ⟦(n, m)⟧
+-- (4, 0) ↦ ⟦(0, 4)⟧
+
+@[notation_simp]
+theorem MyInt.sr_def (m n : PreInt) : m ≈ n ↔ m.1 + n.2 = m.2 + n.1 := by rfl
+
+def MyInt.neg : MyInt → MyInt := Quotient.lift PreInt.neg <| by
+  -- PreInt.neg が同値関係を保つことを示したい
+  -- (a₁, a₂) : PreInt と(b₁, b₂) : PreInt が同値だと仮定する
+  intro (a₁, a₂) (b₁, b₂) hab
+  -- このときneg (a₁, a₂) = neg (b₁, b₂) を示せばよいのだが
+  -- 商空間におけるneg の定義により(a₂, a₁) ≈ (b₂, b₁) を示せばよい
+  suffices (a₂, a₁) ≈ (b₂, b₁) from by
+    dsimp [PreInt.neg]
+    rw [Quotient.sound]
+    assumption
+  -- しかしこれは同値性の定義と仮定より明らか
+  notation_simp at *
+  simp_all
+
+-- MyInt に対して - が来たら、さっき作った MyInt.neg を使ってください
+instance : Neg MyInt where
+neg := MyInt.neg
+
+#check (-4 : MyInt)
+
+-- r はα 上の二項関係とする
+variable {α : Type} {r : α → α → Prop}
+
+private theorem Ex.symm (refl : ∀ x, r x x) (h : ∀ x y z, r x y → r y z → r z x)
+: ∀ {x y : α}, r x y → r y x := by
+  intro x y hxy
+  have h2 : ∀ (x y : α), r x x → r x y → r y x := by
+    intro x y
+    exact h x x y
+  have hxx : r x x := by exact refl x
+  exact h2 x y hxx hxy
+
+private theorem Ex.equiv (refl : ∀ x, r x x)
+(h : ∀ x y z, r x y → r y z → r z x) : Equivalence r := by
+  constructor
+  case refl => exact refl
+  case symm => exact Ex.symm refl h
+  case trans =>
+    intro x y z hxy hyz
+    have hzx := by exact h x y z hxy hyz
+    exact Ex.symm refl h hzx
